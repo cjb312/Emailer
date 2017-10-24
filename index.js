232d65cf7920239ec1f55b22d8f7866aa145365f
Chris Barnes <cjbarnes312@gmail.com>
2017-10-24 17:22:41 -0400

    starting oauth

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-app.get("/", (req, res) => {
-	res.send({ hi: "yes" });
-});
+const app = express();
+// clientId 1001015389641-4jsrfs4l59f33r3f8k4pb3r3co11qrom.apps.googleusercontent.com
+// clientSecret xryaATPX9eFFF7ccGciI7WYG
+
+//Creating new instance of google passport strategy
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
