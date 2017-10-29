78e45be381bd2471f4d883849fc1881c6b9d8a10
Chris Barnes <cjbarnes312@gmail.com>
2017-10-28 21:29:02 -0400

    cookies

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/user"); // Needs to be executed before passport
 require("./services/passport");
 
-
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// Telling express it needs to make use of cookies inside the app
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days * 24 hrs in a day * 60 min in an hour * 60 sec in a min * 1000ms to a second
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Requiring this returns a function that is immediately calls it with app
 require("./routes/authRoutes")(app);
 
