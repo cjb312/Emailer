f35ab09702b04895fcecaf4a5630a38dd34f5f32
Chris Barnes <cjbarnes312@gmail.com>
2017-10-31 19:11:43 -0400

    typos

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,5 @@ if (process.env.NODE_ENV === "production") {
 	module.exports = require("./prod");
 } else {
 	// in development - return the dev keys
-	module.exports = require("/.dev");
+	module.exports = require("./dev");
 }
