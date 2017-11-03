8b7d8dccfe54912c349628e8ae26fc1b138ee7a8
Chris Barnes <cjbarnes312@gmail.com>
2017-11-03 15:53:32 -0400

    added proxy for api

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from "react";
 import ReactDOM from "react-dom";
+import reduxThunk from 'redux-thunk';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 	<Provider store={store}>
