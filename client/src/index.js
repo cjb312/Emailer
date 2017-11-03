a12217db956bcfd61515b3fd28c5d78fdc5d9e9a
Chris Barnes <cjbarnes312@gmail.com>
2017-11-03 11:13:43 -0400

    index reducer imported and set up

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
-	<Provider store={store}><App /></Provider>, 
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.querySelector("#root")
-	);
+);
