c53f0aa1380e1b12bcde3ea5178dd24648999e1c
Chris Barnes <cjbarnes312@gmail.com>
2017-11-03 11:39:14 -0400

    experimenting with always visible components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>dashboard</h2>
+const Survery = () => <h2>survery</h2>
+const Landing = () => <h2>landing</h2>
 
 const App = () => {
-	return (
-		<div>
-			hi there
-		</div>
-		);
+	return(
+	<div>
+		<BrowserRouter>
+			<div>
+				<Route path="/" component={Landing} />
+			</div>
+		</BrowserRouter>
+	</div>
+	);
 };
 
 export default App;
