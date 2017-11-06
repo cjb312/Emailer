2f7a1a70c214e86e27ff315342cfbaabe1046105
Chris Barnes <cjbarnes312@gmail.com>
2017-11-06 11:29:35 -0500

    working on switch case to determine where the verification process is

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
-export default function (state = {}, action) {
-	console.log(action);
+import {FETCH_USER} from '../actions/types';
+
+// verification  will either be null, user model (action.payload), or false
+export default function (state = null, action) {
 	switch (action.type) {
+		case FETCH_USER:
+			return action.payload || false;
 		default:
 			return state;
 	}
