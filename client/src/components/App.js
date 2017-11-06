804faf0ecac4ff53316029c33b6e5d906b3bf62a
Chris Barnes <cjbarnes312@gmail.com>
2017-11-06 10:51:58 -0500

    made app a class based component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 import Header from "./Header";
-const Dashboard = () => <h2>dashboard</h2>
-const SurveyNew = () => <h2>survey</h2>
-const Landing = () => <h2>landing</h2>
+const Dashboard = () => <h2>dashboard</h2>;
+const SurveyNew = () => <h2>survey</h2>;
+const Landing = () => <h2>landing</h2>;
 
-const App = () => {
-	return(
-	<div className="container">
-		<BrowserRouter>
-			<div>
-				<Header />
-				<Route exact path="/" component={Landing} />
-				<Route exact path="/surveys" component={Dashboard} />
-				<Route path="/surveys/new" component={SurveyNew} />
+class App extends Component {
+	//fetching current user once application loads
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
 			</div>
-		</BrowserRouter>
-	</div>
-	);
-};
+		);
+	}
+}
 
-export default App;
+export default connect(null, actions)(App);
