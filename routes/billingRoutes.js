7e00348673b17659e74440a7646a87580561bd88
Chris Barnes <cjbarnes312@gmail.com>
2017-11-07 19:59:58 -0500

    added a middleware to check to see if a user is logged in. if they are then the next function is called otherwise an error is thrown

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,11 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
 	app.post("/api/stripe", async (req, res) => {
+		// sets http status code to an error if no user is found
+		if (!req.user) {
+			return res.status(401).send({error: "You must log in!"});
+		}
+
 		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: "usd",
