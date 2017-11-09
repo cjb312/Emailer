c8f42a317675d3a8e81f420d1e864a34510a906a
Chris Barnes <cjbarnes312@gmail.com>
2017-11-08 19:46:23 -0500

    trying to fix lifecyle error

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 
 app.use(passport.session());
 
-// Requiring this returns a function that is immediately calls it with app
+/*// Requiring this returns a function that is immediately calls it with app
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
@@ -39,6 +39,18 @@ if (process.env.NODE_ENV === "production") {
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
+}*/
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 8080;
