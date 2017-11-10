2bca9607e3cf6548c1037aaee946dc3fefa03919
Chris Barnes <cjbarnes312@gmail.com>
2017-11-10 11:40:28 -0500

    created a schema for recipients

--- models/survey.js
+++ models/survey.js
@@ -1,11 +1,14 @@
+// Seperate collection from users to avoid hitting the 4mb limit if the email list was super long.
+
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const RecipientSchema = require("./recipient");
 
 const surveySchema = new Schema({
 	title: String,
 	body: String,
 	subject: String,
-	recipients: [String],
+	recipients: [recipientSchema],
 	yes: { type: Number, default:0},
 	no: { type: Number, default:0}
 
