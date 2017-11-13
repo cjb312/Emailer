f14d17b93f50cfc3d575a2e79918fb1f0a0a5856
Chris Barnes <cjbarnes312@gmail.com>
2017-11-13 17:07:00 -0500

    structuring mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,9 +1,16 @@
-const sendgrid = require('sendgrid');
+const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
-const keys = require ("../config/keys");
+const keys = require("../config/keys");
 
 class Mailer extends helper.Mail {
+	constructor({ subject, recipients }, content) {
+		super();
 
+		this.from_email = new helper.Email('no-reply@emailer.com');
+		this.subject = subject;
+		this.body = new helper.Context('text/html', content);
+		this.recipients = this.formatAddresses(recipients);
+	}
 }
 
 module.exports = Mailer;
