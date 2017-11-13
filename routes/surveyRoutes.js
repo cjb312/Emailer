3cf8408712adcee9924328fbb429b414dd46971c
Chris Barnes <cjbarnes312@gmail.com>
2017-11-13 14:58:12 -0500

    putting emails into an array of objects

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,11 +8,12 @@ module.exports = app => {
 	app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
-		const survey = new Survey ({
-			title, 
+		const survey = new Survey({
+			title,
 			subject,
 			body,
-			
-		})
+			// Takes the recipients emails splits the string of emails after each comma and maps them to an array of objects of email: asdfadf@gasdf.com
+			recipients: recipients.split(",").map(email => ({email}))
 	});
+	});	
 };
