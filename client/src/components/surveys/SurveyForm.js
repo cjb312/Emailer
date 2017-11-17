6e6787330136ca01a14da06918bc0ec86b8ef81a
Chris Barnes <cjbarnes312@gmail.com>
2017-11-17 12:07:21 -0500

    adding survey components

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,14 @@
+// SurveyForm shows a form for user to add input
+import React, { Component } from 'react';
+
+class SurveyForm extends Component {
+	render() {
+		return (
+			<div>
+				SurveyForm!
+			</div>
+			)
+	}
+}
+
+export default SurveyForm;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import {reducer} from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({ 
