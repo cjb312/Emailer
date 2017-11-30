99e87dbbe64f7ece5927819299aa641c42c97651
Chris Barnes <cjbarnes312@gmail.com>
2017-11-30 11:32:04 -0500

    adding form review

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,29 @@
-//SurveyNew shows SurveyFrom 
-import React, { Component } from 'react';
-import SurveyForm from './SurveyForm';
+//SurveyNew shows SurveyFrom
+import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
+	/*constructor(props) {
+		super(props);
+
+		this.state = { new: true };
+	}*/
+	//shortcut for above
+	state = { showReview: false };
+	// ShowFormReview being set to false either initally brings up the new survey page or when reviewing it is set to false onCancel so the user can navigate back
+	renderContent() {
+		if (this.state.showFormReview === true) {
+			return <SurveyFormReview
+				onCancel={()=>this.setState({showFormReview: false}) }
+			 />;
+		}
+
+		return <SurveyForm onSurveySubmit={() => this.setState({showFormReview:true})} />;
+	}
+
 	render() {
-		return (
-			<div>
-				<SurveyForm />
-			</div>
-			)
+		return <div>{this.renderContent()}</div>;
 	}
 }
 
