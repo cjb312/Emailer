bb4c1bfe701972cdb949db9fa4841b09a99f75f6
Chris Barnes <cjbarnes312@gmail.com>
2017-11-30 12:02:31 -0500

    made form fields its own file for easier importing of the object

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,11 @@ import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
 import { Link } from "react-router-dom";
 import validateEmails from "../../utils/validateEmails";
-
-const FIELDS = [
-	{ label: "Survey Title", name: "title" },
-	{ label: "Subject Line", name: "subject" },
-	{ label: "Email Body", name: "body" },
-	{ label: "Recipient List", name: "emails" }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
 	renderFields() {
-		return _.map(FIELDS, ({ label, name }) => {
+		return _.map(formFields, ({ label, name }) => {
 			return (
 				<Field
 					key={name}
@@ -58,7 +52,7 @@ function validate(values) {
 
 	errors.emails = validateEmails(values.emails || "");
 
-	_.each(FIELDS, ({ name }) => {
+	_.each(formFields, ({ name }) => {
 		if (!values[name]) {
 			errors[name] = "You must provide a value";
 		}
@@ -69,5 +63,7 @@ function validate(values) {
 
 export default reduxForm({
 	validate: validate,
-	form: "surveyForm"
+	form: "surveyForm",
+	destroyOnUnmount:false
 })(SurveyForm);
+//destroyOnUnmount:false keeps data in its fields when a user clicks back on the review form
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,5 +1,6 @@
 //SurveyFormReview shows users their form inputs for review
 import React from "react";
+import { connect } from 'react-redux';
 
 const SurveyFormReview = ({ onCancel }) => {
 	return (
@@ -12,4 +13,9 @@ const SurveyFormReview = ({ onCancel }) => {
 	);
 };
 
-export default SurveyFormReview;
+
+function mapStateToProps(state) {
+  return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
